c4a9cfd75123fad53b8ee87d1dc2c82ca2180d7e
Angelo <u20231B775@upc.edu.pe>
2025-10-09 03:56:40 -0500

    feat(workshop): update request detail screen and add assets

--- app/src/main/java/com/atg/autonexo/features/matchingbooking/presentation/request/RequestDetailScreen.kt
+++ app/src/main/java/com/atg/autonexo/features/matchingbooking/presentation/request/RequestDetailScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.DirectionsCar
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
